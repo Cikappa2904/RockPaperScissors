7fbce5b54137015ff136bd00c069a86998b6da47
Cikappa2904 <cikappa.0429@gmail.com>
2021-12-23 09:30:00 +0100

    added regex for ip address

--- RockPaperScissors/Settings.xaml.cs
+++ RockPaperScissors/Settings.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -29,10 +30,32 @@ namespace RockPaperScissors
             this.InitializeComponent();
         }
 
+        private async void DisplayResultDialog(string title, string content)
+        {
+            //var resourceLoader = Windows.ApplicationModel.Resources.ResourceLoader.GetForCurrentView();
+            //string tempContent = content + " " + resourceLoader.GetString(move) + content2;
+
+            ContentDialog1.Title = title;
+            ContentDialog1.Content = content;
+            ContentDialog1.CloseButtonText = "Ok";
+            ContentDialog1.DefaultButton = ContentDialogButton.Close;
+
+            ContentDialogResult result = await ContentDialog1.ShowAsync();
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            localSettings.Values["ipAddress"] = serverIP.Text;
+            Regex ipRegex = new Regex("([0-9]{1,3}(?:\\.[0-9]{1,3}){3}|[a-zA-Z]+):([0-9]{1,5})"); //regex stolen from https://stackoverflow.com/questions/57288837/regex-if-valid-websocket-address 
+            if (ipRegex.IsMatch(serverIP.Text))
+            {
+                localSettings.Values["ipAddress"] = serverIP.Text;
+            }
+            else
+            {
+                DisplayResultDialog("not valid IP", "The IP you inserted is not valid");
+            }
+            
+
         }
 
     }
